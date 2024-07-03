3b129777767fe2387809ca0c98f877235a58ac4a
b4baxxx <zidanyyu@gmail.com>
2024-07-03 12:53:02 +0700

    .

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -16,7 +16,7 @@ class AuthenticatedSessionController extends Controller
      */
     public function create(): View
     {
-        return view('auth.loginnew');
+        return view('auth.login');
     }
 
     /**
