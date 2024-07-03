f462c07a9afbfeb88d35140c4e2eb62a0f571acd
b4baxxx <zidanyyu@gmail.com>
2024-07-03 12:27:31 +0700

    p

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -19,7 +19,7 @@ class RegisteredUserController extends Controller
      */
     public function create(): View
     {
-        return view('auth.registernew');
+        return view('auth.register');
     }
 
     /**
